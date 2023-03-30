ccc3d8320e1dd3406de5fc380412a8cc9f02951a
EduardRadeanu <ediradeanu12345@gmail.com>
2023-03-30 12:14:21 +0300

    task1

--- proiect4_v2/Program.cs
+++ proiect4_v2/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿public class Car
+{
+    public int Id;
+    public string Brand;
+    public string Model;
+    public bool IsElectric;
+    public int MaxSpeedKmh;
+    public int AutonomyKm;
+}
